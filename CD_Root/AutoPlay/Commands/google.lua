1bf4f6c08cd8b63018279fbe2c5a43790c242b42
Leka74 <leke.dobruna@gmail.com>
2011-05-21 16:35:17 +0200

    Added the wikipedia search command Signed-off-by: Leka74 <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/google.lua
+++ CD_Root/AutoPlay/Commands/google.lua
@@ -1,6 +1,10 @@
 CMD.AddCommand("google", "Search in google for something.", function(Arguments)
-	local Args = Table.Concat(Arguments, " ", 1, -1);
 	-- Example: google Cookies
-	File.OpenURL("http://www.google.com/search?q="..Args, SW_SHOWNORMAL);
-	CMD.Display("Searching in Google for: "..Args);
+	local Args = Table.Concat(Arguments, " ", 1, -1);
+	if Args ~= "" and Args ~= " " then 	
+		File.OpenURL("http://www.google.com/search?q="..Args, SW_SHOWNORMAL);
+		CMD.Display("Searching in Google for: "..Args);
+	else
+		CMD.Display("Syntax missing. Usage: google <text>");
+	end
 end);
--- CD_Root/AutoPlay/Commands/youtube.lua
+++ CD_Root/AutoPlay/Commands/youtube.lua
@@ -1,6 +1,10 @@
 CMD.AddCommand("youtube", "Search in youtube for a video.", function(Arguments)
-	local Args = Table.Concat(Arguments, " ", 1, -1);
 	-- Example: youtube 2012
-	File.OpenURL("http://www.youtube.com/results?search_query="..Args, SW_SHOWNORMAL);
-	CMD.Display("Searching in Youtube for: "..Args);
+	local Args = Table.Concat(Arguments, " ", 1, -1);
+	if Args ~= "" and Args ~= " " then
+		File.OpenURL("http://www.youtube.com/results?search_query="..Args, SW_SHOWNORMAL);
+		CMD.Display("Searching in Youtube for: "..Args);
+	else
+		CMD.Display("Syntax missing. Usage: youtube <text>");
+	end
 end);
