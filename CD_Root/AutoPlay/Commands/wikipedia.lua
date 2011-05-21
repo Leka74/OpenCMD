c134c8e5eb161c6a9898e299e9f2ceeab4e74dd2
Leka74 <leke.dobruna@gmail.com>
2011-05-21 16:36:45 +0200

    Added the wikipedia command. Signed-off-by: Leka74 <leke.dobruna@gmail.com>

---
+++ CD_Root/AutoPlay/Commands/wikipedia.lua
@@ -0,0 +1,12 @@
+CMD.AddCommand("wikipedia", "Search in wikipedia for something.", function(Arguments)
+	-- Example: wikipedia Google
+	local Args = Table.Concat(Arguments, " ", 1, -1);
+	if Args ~= "" and Args ~= " " then 
+		File.OpenURL("http://en.wikipedia.org/wiki/Special:Search/"..Args, SW_SHOWNORMAL);
+		CMD.Display("Searching in Wikipedia for: "..Args);
+	else
+		CMD.Display("Syntax missing. Usage: wikipedia <text>");
+	end
+end);
+
+CMD.AddAlias("wikipedia", "wiki");
