f643e6b8f19e3a7d0def1b64120425092349d729
Leka74 <leke.dobruna@gmail.com>
2011-05-22 18:39:49 +0200

    Fixed a bug in the temperature command. Signed-off-by: Leka74 <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/weather.lua
+++ CD_Root/AutoPlay/Commands/weather.lua
@@ -1,13 +1,28 @@
-CMD.AddCommand("temperature", "Gives you the temperature in a desired city", function(Arguments)
+CMD.AddCommand("temperature", "Gives you the temperature of a desired city", function(Arguments)
 	-- Example: temperature London England
-	if Table.Count(Arguments) ~= 1 then
+	if Table.Count(Arguments) ~= 1 and Table.Count(Arguments) ~= 0 then
 		if Arguments[1] ~= "" and Arguments[1] ~= " " and Arguments[2] ~= "" and Arguments[2] ~= " " then 	
+			if Table.Count(Arguments) == 3 then
+				Arguments[1] = Arguments[1].."%%20"..Arguments[2];
+				Arguments[2] = Arguments[3];
+			elseif Table.Count(Arguments) == 4 then
+				Arguments[1] = Arguments[1].."%%20"..Arguments[2];
+				Arguments[2] = Arguments[3].."%%20"..Arguments[4];
+			end
+			
+			
 			local xml = http.request("http://www.google.com/ig/api?weather="..Arguments[1]..",%%20"..Arguments[2]);
 			if xml ~= nil then
 				XML.SetXML(xml);
 				TextFile.WriteFromString(_DesktopFolder.."\\MyFile.txt", xml, false);
 				local intCelsius = XML.GetAttribute("xml_api_reply/weather/current_conditions/temp_c", "data");
-				CMD.Display("The temperature in "..Arguments[1].." is "..intCelsius.." celsius degrees.");
+				if intCelsius ~= "" then
+					Arguments[1] = String.Replace(Arguments[1], "%%20", " ", false);
+					Arguments[2] = String.Replace(Arguments[2], "%%20", " ", false);
+					CMD.Display("The temperature in "..Arguments[1].." is "..intCelsius.." celsius degrees.");
+				else
+					CMD.Display("Invalid City or Country.");
+				end
 			else
 				CMD.Display("No internet connection detected.");
 			end
