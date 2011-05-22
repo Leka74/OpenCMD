e5fbc64d2ca6fa7acc33ac507e29f3a91a9e0808
Leka74 <leke.dobruna@gmail.com>
2011-05-22 20:38:43 +0200

    Over-writing an old command is no longer allowed via get. Signed-off-by: Leka74 <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/get.lua
+++ CD_Root/AutoPlay/Commands/get.lua
@@ -1,20 +1,24 @@
 CMD.AddCommand("get", "Downloads a command.", function(Arguments)
 	local Args = Table.Concat(Arguments, " ", 1, -1);
 	if Args ~= "" and Args ~= " " then
+		CMD.Display("Downloading the command...");
 		local commandcode = http.request(Args);
 		if commandcode ~= nil then	
 			local tbFile = String.SplitPath(Args);
 			if tbFile.Extension == ".lua" then
-				CMD.Display("Downloading the command...");
-				TextFile.WriteFromString("AutoPlay\\Commands\\"..tbFile.Filename..".lua", commandcode, false);
-				CMD.Display("Command downloaded. Installing...");
-				dofile("AutoPlay\\Commands\\"..tbFile.Filename..".lua");
-				CMD.Display("Command installed.");
+				if File.DoesExist("AutoPlay\\Commands\\"..tbFile.Filename..".lua") then
+					CMD.Display("Error downloading. Command with the same filename already exists");
+				else
+					TextFile.WriteFromString("AutoPlay\\Commands\\"..tbFile.Filename..".lua", commandcode, false);
+					CMD.Display("Command downloaded. Installing...");
+					dofile("AutoPlay\\Commands\\"..tbFile.Filename..".lua");
+					CMD.Display("Command installed.");
+				end
 			else
-				CMD.Display("Wrong file format.");
+				CMD.Display("Downloading failed. Wrong file format.");
 			end
 		else
-			CMD.Display("Wrong link or no internet connection.");
+			CMD.Display("Downloading failed. Wrong link or no internet connection.");
 		end
 	else
 		CMD.Display("Syntax missing. Usage: get <link>");
