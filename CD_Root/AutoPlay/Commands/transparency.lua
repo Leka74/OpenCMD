3a46b500a48d133dd7ec08e824b417a3130fa612
Leka74 <leke.dobruna@gmail.com>
2011-05-24 19:10:44 +0200

    The transparency command now saves the stats in the config file. Signed-off-by: Leka74 <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/transparency.lua
+++ CD_Root/AutoPlay/Commands/transparency.lua
@@ -2,10 +2,12 @@ CMD.AddCommand("transparency", "Change the window transparency.", function(Argum
 	if Table.Concat(Arguments, " ", 1, -1) == "on" then
 		-- Example: transparency on
 		WinApi.SetWindowTrans(Application.GetWndHandle(), 200);
+		INIFile.SetValue("AutoPlay\\config.ini", "Configuration", "Transparency", "200");
 		CMD.Display("Transparency enabled.");
 	elseif Table.Concat(Arguments, " ", 1, -1) == "off" then
 		-- Example: transparency off
 		WinApi.SetWindowTrans(Application.GetWndHandle(), 255);
+		INIFile.SetValue("AutoPlay\\config.ini", "Configuration", "Transparency", "255");
 		CMD.Display("Transparency disabled.");
 	else
 		CMD.Display("Transparency: Unknown state.");
