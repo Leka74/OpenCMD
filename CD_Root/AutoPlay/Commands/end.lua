06dec580686fea566cfdb0a4bb1ba52bfd3219e8
Leke Dobruna <leke.dobruna@gmail.com>
2011-05-25 19:47:09 +0200

    Added a 'Syntax missing' error to the end command.
    Signed-off-by: Leke Dobruna <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/end.lua
+++ CD_Root/AutoPlay/Commands/end.lua
@@ -1,9 +1,13 @@
 CMD.AddCommand("end", "End a process.", function(Arguments)
-	local Args = Table.Concat(Arguments, " ", 1, -1);
 	-- Example: end itunes.exe
-	if System.EndProcess(Args) then
-		CMD.Display(Args.." process has ended.");
+	local Args = Table.Concat(Arguments, " ", 1, -1);
+	if (String.Replace(Args, " ", "", false) ~= "") then
+		if System.EndProcess(Args) then
+			CMD.Display(Args.." process has ended.");
+		else
+			CMD.Display("The process "..Args.." wasn't found.");
+		end
 	else
-		CMD.Display("The process "..Args.." wasn't found.");
+		CMD.Display("Syntax missing. Usage: end <process>");
 	end
 end);
