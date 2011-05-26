f7cc9fecc2f0cd6abbd2d94e7a7c260e26223419
Leke Dobruna <leke.dobruna@gmail.com>
2011-05-26 16:34:35 +0200

    Updated the IP file link. Thanks to Rexzooly for hosting it. Signed-off-by: Leke Dobruna <leke.dobruna@gmail.com>

--- CD_Root/AutoPlay/Commands/ip.lua
+++ CD_Root/AutoPlay/Commands/ip.lua
@@ -1,8 +1,10 @@
 CMD.AddCommand("ip", "Shows your current public IP address", function()
-	local ip = CMD.HTTPSubmit("http://uvlabs.co.cc/vikisystem/getip.php");
+	local ip = CMD.HTTPSubmit("http://feeder.dualblue.co.uk/app.php");
 	if ip then
 		CMD.Display("Your current IP address is: "..ip);
 	else
 		CMD.Display("No internet connection detected.");
 	end
 end);
+
+-- Thanks to Rexzooly for hosting the IP file.
